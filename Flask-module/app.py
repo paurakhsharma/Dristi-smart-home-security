1e578141338008922fa1e9e135ef0579b96bc772
paurakhsharma <paurakh011@gmail.com>
2018-06-03 08:02:43 +0545

    Implement websocket in recogniser

--- Flask-module/app.py
+++ Flask-module/app.py
@@ -3,14 +3,24 @@
 import sys
 sys.path.append('../')
 
-from flask import Flask, jsonify, abort, make_response,request
+from flask import Flask, jsonify, render_template, session, request
 from flask_httpauth import HTTPBasicAuth
+from flask_socketio import SocketIO, emit
+
+
+from threading import Lock
 
 from facialRecognition import dataSetCreator,trainer,recogniser
 
 auth = HTTPBasicAuth()
+async_mode = "threading"
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, async_mode=async_mode)
+
+
+
 
 @auth.get_password
 def get_password(username):
@@ -18,9 +28,9 @@ def get_password(username):
         return 'sublimeuser'
     return None
 
-@auth.error_handler
-def unauthorized():
-    return make_response(jsonify({'error': 'Unauthorized access'}), 403)    
+# @auth.error_handler
+# def unauthorized():
+    # return make_response(jsonify({'error': 'Unauthorized access'}), 403) 
 
 #Run dataSetCreator script
 @app.route('/flask/api/v1.0/create/<int:id>', methods=['GET'])
@@ -39,59 +49,13 @@ def train_dataSet():
 def recognise_faces():
     return(recogniser.reconizer_func())
 
-# @app.errorhandler(404)
-# def not_found(error):
-#     return make_response(jsonify({'error': 'Not found'}), 404)
+@app.route("/")
+def index():
+    return render_template('index.html',)
 
-# #Run dataSetCreator script for a given username
-# @app.route('/todo/api/v1.0/task', methods=['POST'])
-# @auth.login_required
-# def create_task():
-#     if not request.json or not 'title' in request.json:
-#         abort(400)
-#     task = {
-#         'id': tasks[-1]['id']+1,
-#         #Title is the required key
-#         'title': request.json['title'],
-#         #Discription is not an required key
-#         'description': request.json.get('description'),
-#         'done': False
-#     }
-#     tasks.append(task)
-#     return jsonify({'task': task}), 201
-
-# #Edit a username
-# @app.route('/todo/api/v1.0/task/<int:task_id>', methods=['PUT'])
-# @auth.login_required
-# def update_task(task_id):
-#     task = [task for task in tasks if task['id'] == task_id]
-#     if len(task) == 0:
-#         abort(404)
-#     if not request.json:
-#         abort(400)
-#     if 'title' in request.json and type(request.json['title']) != str:
-#         abort(400)
-#     if 'description' in request.json and type(request.json['description']) is not str:
-#         abort(400)
-#     if 'done' in request.json and type(request.json['done']) is not bool:
-#         abort(400)
-
-#     task[0]['title'] = request.json.get('title', task[0]['title'])
-#     task[0]['description'] = request.json.get('description', task[0]['description'])                
-#     task[0]['done'] = request.json.get('done', task[0]['done'])
-
-#     return jsonify({'task':task[0]})
-
-# #Deleta an user
-# @app.route('/flask/api/v1.0/task/<int:task_id>', methods=['DELETE'])
-# @auth.login_required
-# def delete_task(task_id):
-#     task = [task for task in tasks if task['id'] == task_id]
-#     if len(task) == 0:
-#         abort(404)
-#     tasks.remove(task[0])
-
-#     return jsonify({'result': True})    
+@socketio.on('send_message')
+def handle_source():
+    recogniser.reconizer_func()
 
 if __name__== '__main__':
-    app.run(debug=True, threaded=True)
+    socketio.run(app,debug=True)
